bd9b6e1720b159c6a3ae761dceed0c1d43eddcd9
Fatih Gürdal <fgurdal@bimser.com.tr>
2021-12-19 15:18:02 +0300

    migration

---
+++ LinkConverter.DataAccess.Postgresql/Migrations/20211219121734_HistoryTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace LinkConverter.Repository.Postgresql.Migrations
+{
+    public partial class HistoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LINKCONVERT_HISTORIES",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CONVERT_TYPE = table.Column<byte>(nullable: false),
+                    REQUEST_LINK = table.Column<string>(nullable: false),
+                    RESPONSE_LINK = table.Column<string>(nullable: false),
+                    Date = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LINKCONVERT_HISTORIES", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LINKCONVERT_HISTORIES");
+        }
+    }
+}
